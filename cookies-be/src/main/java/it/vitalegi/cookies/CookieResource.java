4426f4843d70424aa731ab0f7769b5951d91e752
Giorgio Vitale <giorgio.vitale.90@gmail.com>
2023-09-20 21:18:32 +0200

    sample code with localStorage (fails); sessionStorage (fails); postMessage (works)

--- cookies-be/src/main/java/it/vitalegi/cookies/CookieResource.java
+++ cookies-be/src/main/java/it/vitalegi/cookies/CookieResource.java
@@ -1,5 +1,6 @@
 package it.vitalegi.cookies;
 
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,14 +17,14 @@ public class CookieResource {
 
     Logger log = LoggerFactory.getLogger(getClass());
 
-    @GetMapping("/test1")
-    public Cookies getOrInitCookies(@CookieValue(name = "cookie1", required = false) String cookie, HttpServletResponse response) {
-        log.info("Received cookie: {}", cookie);
+    @GetMapping("/{id}")
+    public Cookies getOrInitCookies(@PathVariable("id") String id, @CookieValue(name = "cookie1", required = false) String cookie, HttpServletRequest request, HttpServletResponse response) {
+        log.info("{}. received cookie: {}. Origin={}", id, cookie, request.getHeader("Origin"));
         if (cookie != null && !cookie.isBlank()) {
             return cookies(cookie);
         }
         var newValue = UUID.randomUUID().toString();
-        log.info("Set cookie {}={}", "cookie1", newValue);
+        log.info("{} Set cookie {}={}", id, "cookie1", newValue);
         var newCookie = "cookie1=" + newValue + "; Domain=.company.eu; SameSite=None; Secure; HttpOnly; Path=/; Max-Age=1600";
         response.addHeader(HttpHeaders.SET_COOKIE, newCookie);
         return cookies(newValue);
